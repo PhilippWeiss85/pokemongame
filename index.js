4bc3b6b9a451ad34705f5e45709c6620820ee373
Philipp Weiß <p.weiss85@gmx.de>
2022-12-06 12:09:22 +0100

    update lastkeypressed

--- index.js
+++ index.js
@@ -84,32 +84,32 @@ function animate() {
     playerImage.height
     )
 
-    if(keys.w.pressed) background.position.y += 3;
-    else if(keys.a.pressed) background.position.x += 3;
-    else if(keys.d.pressed) background.position.x -= 3;
-    else if(keys.s.pressed) background.position.y -= 3;
-    console.log(background.position.y)
+    if(keys.w.pressed && lastKey === "w") background.position.y += 3;
+    else if(keys.a.pressed && lastKey === "a") background.position.x += 3;
+    else if(keys.d.pressed && lastKey === "d") background.position.x -= 3;
+    else if(keys.s.pressed && lastKey === "s") background.position.y -= 3;
+    console.log("test", background.position.y)
 }
 animate()
 
-
+let lastKey = ""
 window.addEventListener('keydown', (event) => {
     switch(event.key) {
         case "w":
         keys.w.pressed = true;
-  
+        lastKey = "w"
         break
         case "a":
         keys.a.pressed = true;
-  
+        lastKey = "a"
         break
         case "d":
         keys.d.pressed = true;
-
+        lastKey = "d"
         break
         case "s":
         keys.s.pressed = true;
-
+        lastKey = "s"
         break
     }
 
