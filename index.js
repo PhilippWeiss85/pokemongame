d9658e74c0be80040b6bedccd7b8fb840038f704
Philipp Weiß <p.weiss85@gmx.de>
2022-12-06 10:59:31 +0100

    add keydown eventlistener

--- index.js
+++ index.js
@@ -5,13 +5,47 @@ canvas.width = 1024
 canvas.height = 576
 c.fillStyle = 'white'
 console.log(c);
-
 c.fillRect(0, 0, canvas.width, canvas.height)
+
+
 const image = new Image();
 image.src = './img/Town.png'
 
-console.log(image)
+const playerImage = new Image();
+playerImage.src = "./img/playerDown.png"
 
 image.onload = () => {
-    c.drawImage(image, -1000, -500)
+    c.drawImage(image, -1025, -580)
+    c.drawImage(playerImage,
+    0,
+    0,
+    playerImage.width / 4,
+    playerImage.height,
+    canvas.width / 2 - (playerImage.width / 4 / 2),
+    canvas.height / 2 - playerImage.height / 2,
+    playerImage.width / 4,
+    playerImage.height
+    )
 }
+
+
+
+
+
+
+window.addEventListener('keydown', (event) => {
+    switch(event.key) {
+        case "w":
+        console.log(event.key)
+        break
+        case "a":
+        console.log(event.key)
+        break
+        case "d":
+        console.log(event.key)
+        break
+        case "s":
+        console.log(event.key)
+        break
+    }
+})
