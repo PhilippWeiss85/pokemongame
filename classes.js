66eaf84e238976a3732efe7e0bf407d251d8dbbb
Philipp Weiß <p.weiss85@gmx.de>
2022-12-19 11:30:28 +0100

    add attack interface

--- classes.js
+++ classes.js
@@ -50,7 +50,7 @@ class Sprite {
     }
   
     draw() {
-      c.fillStyle = "rgba(255, 0, 0, 0.5)";
+      c.fillStyle = "rgba(255, 0, 0, 0)";
       c.fillRect(this.position.x, this.position.y, this.width, this.height);
     }
   }
